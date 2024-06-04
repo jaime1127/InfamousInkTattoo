9775ffb1d9ed92ca6fe12ddee3cc60fe124d9ed1
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2024-06-04 00:24:52 -0400

    updates to the header component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,14 @@
 	import Header from '$lib/Components/Header.svelte';
 	import Footer from '$lib/Components/Footer.svelte';
 	import Hero from '$lib/Components/Hero.svelte';
+	import Testimonal from '$lib/Components/Testimonal.svelte';
+	import GridList from '$lib/Components/GridList.svelte';
 </script>
 
 <Header />
 <main>
 	<Hero />
-	<slot />
+	<GridList />
+	<Testimonal />
 </main>
 <Footer />
-
-<style>
-</style>
