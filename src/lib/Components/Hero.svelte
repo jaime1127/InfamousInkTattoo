07b875cd9a34c2c2258e5ebcc5be24688c2540ab
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2024-06-04 15:34:47 -0400

    changes to video component

--- src/lib/Components/Hero.svelte
+++ src/lib/Components/Hero.svelte
@@ -1,26 +1,9 @@
-<section class="relative isolate overflow-hidden bg-gray-900 pb-16 pt-14 sm:pb-20">
-    <div class="absolute inset-0 -z-10 overflow-hidden">
-        <!-- <iframe
-            class="absolute inset-0 -z-10 h-full w-full object-cover"
-            src="https://www.youtube.com/embed/TP8N1a-edBE?autoplay=1&mute=1&loop=1&playlist=TP8N1a-edBE"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-		
-			title=""
-        ></iframe> -->
-		<img
-		src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-		alt=""
-		class="absolute inset-0 -z-10 h-full w-full object-cover"
-	/>
-    </div>
-    <div class="relative mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div class="text-center">
-            <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Infamous Tattoo</h1>
-            <p class="mt-6 text-lg leading-8 text-gray-300">
-                At Infamous Ink Tattoos and Piercing, we pride ourselves on being more than just another
-                tattoo parlor.
-            </p>
-        </div>
-    </div>
+<script lang="ts">
+	import vid from '$lib/video/vid.mp4';
+</script>
+
+<section class="relative min-h-screen flex">
+	<video class="absolute top-0 left-0" autoplay loop muted playsinline>
+		<source src={vid} type="video/mp4" />
+	</video>
 </section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang='ts'>
 	import '../app.css';
 	import Header from '$lib/Components/Header.svelte';
 	import Footer from '$lib/Components/Footer.svelte';
