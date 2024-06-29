26b0d79c515edadeab282a52bbccff001ef9f098
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2024-06-29 14:34:41 -0400

    remove footer links

--- src/lib/Components/Footer.svelte
+++ src/lib/Components/Footer.svelte
@@ -3,13 +3,6 @@
 	import Instagram from '$lib/Svg/Instagram.svelte';
 	import Twitter from '$lib/Svg/Twitter.svelte';
 
-	let items = [
-		{ label: 'About', href: '/aboutus' },
-		{ label: 'Artists', href: '/artists' },
-		{ label: 'Portfolio', href: '/portfolio' },
-		{ label: 'About Us', href: '/aboutus' }
-	];
-
 	let socials = [
 		{
 			component: Facebook,
@@ -31,16 +24,7 @@
 
 <footer class="bg-gray-900">
 	<div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-		<nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
-			{#each items as item}
-				<div class="pb-6">
-					<a href={item.href} class="text-sm leading-6 text-gray-400 hover:text-gray-500">
-						{item.label}
-					</a>
-				</div>
-			{/each}
-		</nav>
-		<div class="mt-10 flex justify-center space-x-10">
+		<div class=" flex justify-center space-x-10">
 			{#each socials as social}
 				<a href={social.href} class="text-gray-400 hover:text-gray-500">
 					<span class="sr-only">{social.label}</span>
