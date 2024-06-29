958d83a287e869bb39e37b732ffdde4e53e62fcd
jaime1127 <84351799+jaime1127@users.noreply.github.com>
2024-06-29 14:43:45 -0400

    image fix v7

--- src/lib/Components/Video.svelte
+++ src/lib/Components/Video.svelte
@@ -2,14 +2,12 @@
 	<video class="absolute top-0 left-0 w-full h-full object-cover" autoplay loop muted playsinline>
 		<source src="video/heroVideo.mp4" type="video/mp4" />
 	</video>
-	<div class="absolute inset-0 flex justify-center items-center">
-		<div class="lg:w-full">
-			<img
-				class="object-contain w-full h-full"
-				loading="lazy"
-				src="images/logo/infamous.png"
-				alt="logo overlay"
-			/>
-		</div>
+	<div class="absolute inset-0 flex justify-center self-center">
+		<img
+			class="w-full md:w-3/4 lg:w-1/2"
+			loading="lazy"
+			src="images/logo/infamous.png"
+			alt="logo overlay"
+		/>
 	</div>
 </section>
