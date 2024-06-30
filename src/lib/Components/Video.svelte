681efbc43318cb295ff4fa3c5e40fd20fb5468e4
Obie <obiemunozjr@gmail.com>
2024-06-29 20:07:32 -0400

    fixed image video

--- src/lib/Components/Video.svelte
+++ src/lib/Components/Video.svelte
@@ -1,13 +1,11 @@
-<section class="relative min-h-lvh">
+<section class="flex items-center justify-center min-h-screen">
 	<video class="absolute top-0 left-0 w-full h-full object-cover" autoplay loop muted playsinline>
 		<source src="video/heroVideo.mp4" type="video/mp4" />
 	</video>
-	<div class="absolute inset-0 flex justify-center self-center">
-		<img
-			class="md:w-3/4 lg:w-1/2"
-			loading="lazy"
-			src="images/logo/infamous.png"
-			alt="logo overlay"
-		/>
-	</div>
+	<img
+		class="absolute md:w-3/4 lg:w-1/2"
+		loading="lazy"
+		src="images/logo/infamous.png"
+		alt="logo overlay"
+	/>
 </section>
